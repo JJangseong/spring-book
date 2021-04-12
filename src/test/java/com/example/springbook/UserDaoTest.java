d1db14d86b75bfdd531fc412757092955bd58b86
Jangseongjin <sungjin5891@gmail.com>
2021-04-12 20:18:49 +0900

    Updated UserDaoTest

--- src/test/java/com/example/springbook/UserDaoTest.java
+++ src/test/java/com/example/springbook/UserDaoTest.java
@@ -19,10 +19,13 @@ public class UserDaoTest {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
         User user = getUser();
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록 성공");
+        assertThat(dao.getCount(), is(1));
 
         User user2 = dao.get(user.getId());
 
