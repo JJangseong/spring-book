a74fcd7ee1ae4e071013296e0261e56730e68cad
Jangseongjin <sungjin5891@gmail.com>
2021-04-15 21:13:24 +0900

    Updated JUnitTest Method

--- src/test/java/com/example/springbook/JUnitTest.java
+++ src/test/java/com/example/springbook/JUnitTest.java
@@ -2,27 +2,36 @@ package com.example.springbook;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsIterableContaining.hasItem;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsSame.sameInstance;
 
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 
-    @Test public void test() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+    @Test
+    public void test() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
-    @Test public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+    @Test
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+
     }
 
-    @Test public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+    @Test
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+       
     }
 
 }
