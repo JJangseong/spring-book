feb9ddfbcdf296308595dc0306e54eb78840eb42
Jangseongjin <sungjin5891@gmail.com>
2021-04-15 21:23:51 +0900

    Updated JUnitTest Method

--- src/test/java/com/example/springbook/JUnitTest.java
+++ src/test/java/com/example/springbook/JUnitTest.java
@@ -1,23 +1,41 @@
 package com.example.springbook;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.CombinableMatcher.either;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsIterableContaining.hasItem;
 import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.hamcrest.core.IsSame.sameInstance;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
 public class JUnitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = this.context;
     }
 
     @Test
@@ -25,13 +43,17 @@ public class JUnitTest {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
 
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertThat(testObjects, not(hasItem(this)));
         testObjects.add(this);
-       
+
+        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+        contextObject = this.context;
     }
 
 }
