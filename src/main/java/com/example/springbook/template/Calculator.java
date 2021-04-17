b8d24a33c13ca313fcddd411fb91f82979914b78
Jangseongjin <sungjin5891@gmail.com>
2021-04-17 12:28:29 +0900

    콜백 함수 활용 템플릿 추가

---
+++ src/main/java/com/example/springbook/template/Calculator.java
@@ -0,0 +1,33 @@
+package com.example.springbook.template;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+    public Integer calcSum(String filepath) throws IOException {
+        return fileReadTemplate(filepath, (br) -> {
+            Integer sum = 0;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                sum += Integer.valueOf(line);
+            }
+            return sum;
+        });
+    }
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+        BufferedReader br = null;
+
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            if (br != null) { try { br.close(); } catch (IOException e) { System.out.println(e.getMessage()); }}
+        }
+    }
+}
--- src/test/java/com/example/springbook/CalcSumTest.java
+++ src/test/java/com/example/springbook/CalcSumTest.java
@@ -1,12 +1,25 @@
 package com.example.springbook;
 
+import com.example.springbook.template.Calculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 import java.io.IOException;
 
 public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @BeforeEach
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("numbers.txt").getPath();
+    }
+
     @Test
     public void sumOfNumbers() throws IOException {
-
+        assertThat(calculator.calcSum(this.numFilepath), is(10));
     }
 }
