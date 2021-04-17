be7c6a54319f029741c5c75520f2c6966ab41d85
Jangseongjin <sungjin5891@gmail.com>
2021-04-17 11:24:15 +0900

    deleteAll 메소드에 템플릿/콜백 패턴 적용

---
+++ src/test/java/com/example/springbook/CalcSumTest.java
@@ -0,0 +1,12 @@
+package com.example.springbook;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class CalcSumTest {
+    @Test
+    public void sumOfNumbers() throws IOException {
+
+    }
+}
