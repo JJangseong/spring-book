50fe29cf83e2e0958f7162ebb9a5acaa0ee80085
Jangseongjin <sungjin5891@gmail.com>
2021-04-21 21:00:14 +0900

    Add UpdateTest

--- src/test/java/com/example/springbook/UserDaoTest.java
+++ src/test/java/com/example/springbook/UserDaoTest.java
@@ -59,7 +59,7 @@ public class UserDaoTest {
 
         User userGet2 = dao.get(user2.getId());
         checkSameUser(userGet2, user2);
-        
+
     }
 
     @Test
@@ -118,6 +118,29 @@ public class UserDaoTest {
 
         dao.add(user1);
         assertThrows(DuplicateKeyException.class, () -> dao.add(user1));
+    }
+
+    @Test
+    public void update() {
+        dao.deleteAll();
+
+        dao.add(user1);
+        dao.add(user2);
+
+        user1.setName("Hello");
+        user1.setPassword("password");
+        user1.setLevel(Level.GOLD);
+        user1.setLogin(1000);
+        user1.setRecommend(999);
+
+
+        dao.update(user1);
+
+        User user1update = dao.get(user1.getId());
+        checkSameUser(user1, user1update);
+        User user2same = dao.get(user2.getId());
+        checkSameUser(user2, user2same);
+
     }
 
 //    @Test
