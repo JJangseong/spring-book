8d95ffd5e515fbb6905c56060a16359d178be3a2
sungjin96 <sungjin5891@gmail.com>
2021-09-11 15:41:06 +0900

    chpater 1-5

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,10 +1,20 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by marathoner on 2021/09/10
  */
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
-        return new UserDao(new DConnectionMaker());
+        return new UserDao(connectionMaker());
+    }
+
+    @Bean
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
