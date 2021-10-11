a47accf8fb965531f2746d5530f4d74e2e27d789
sungjin96 <sungjin5891@gmail.com>
2021-10-11 19:25:22 +0900

    chpater 1-8

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -2,6 +2,9 @@ package springbook.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * Created by marathoner on 2021/09/10
@@ -11,12 +14,18 @@ public class DaoFactory {
     @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setConnectionMaker(dataSource());
         return userDao;
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
-        return new DConnectionMaker();
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setUrl("jdbc://mysql://localhost/springbook");
+        dataSource.setUsername("root");
+        dataSource.setPassword("password");
+
+        return dataSource;
     }
 }
